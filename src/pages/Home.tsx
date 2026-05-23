9546d9540f564e2ba7c01fc5f17f2dd8e6b6548d
ByteCrafter-rgb <ayushwork983@gmail.com>
2026-05-23 18:35:02 +0530

    responsice enhanced part ii

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,95 +1,41 @@
 import { HeroSection } from "../components/sections/middle/HeroSection";
 import { UploadSection } from "../components/sections/middle/UploadSection";
-
-import { SavedCarousel } from
-"../components/memes/history/SavedCarousel";
+import { SavedCarousel } from "../components/memes/history/SavedCarousel";
 
 function Home() {
-
     return (
-
         <main className="w-full">
-
-            <section className="
-                w-full
-                h-screen
-                flex
-            ">
-
-                <div className="
-                    w-1/2
-                    h-full
-                    bg-[#3e5c76]
-                ">
-
+            {/* Split Section: Stacks on mobile, splits 50/50 on desktop */}
+            <section className="w-full min-h-screen md:h-screen flex flex-col md:flex-row">
+                
+                {/* Left/Top Column: Hero */}
+                <div className="w-full md:w-1/2 min-h-[50vh] md:h-full bg-[#3e5c76]">
                     <HeroSection />
-
                 </div>
 
-                <div className="
-                    w-1/2
-                    h-full
-                    bg-[#b6a6ca]
-                ">
-
+                {/* Right/Bottom Column: Upload Container */}
+                <div className="w-full md:w-1/2 min-h-[50vh] md:h-full bg-[#b6a6ca]">
                     <UploadSection />
-
                 </div>
 
             </section>
 
-            <section className="
-                w-full
-                min-h-screen
-                bg-gradient-to-b
-                from-[#433e3f]
-                to-[#070A13]
-                px-12
-                py-20
-            ">
-
-                <div className="
-                    max-w-7xl
-                    mx-auto
-                ">
-
-                    <h2 className="
-                        text-6xl
-                        font-bold
-                        text-white
-                        tracking-tight
-                    ">
-
+            {/* Gallery Section */}
+            <section className="w-full min-h-screen bg-gradient-to-b from-[#433e3f] to-[#070A13] px-6 sm:px-12 py-16 sm:py-20">
+                <div className="max-w-7xl mx-auto">
+                    <h2 className="text-4xl sm:text-6xl font-bold text-white tracking-tight">
                         Community Memes
-
                     </h2>
-
-                    <p className="
-                        mt-4
-                        text-xl
-                        text-zinc-400
-                    ">
-
+                    <p className="mt-4 text-lg sm:text-xl text-zinc-400">
                         React with memes created by users.
-
                     </p>
-
-                    <div className="
-                        mt-16
-                    ">
-
+                    <div className="mt-12 sm:mt-16">
                         <SavedCarousel />
-
                     </div>
-
                 </div>
-
             </section>
-
         </main>
-
     );
-
 }
 
 export default Home;
